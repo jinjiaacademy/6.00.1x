ef6f8b6e778f8398c0b603e34651422ddb9e2cad
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-02-05 20:54:19 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -199,3 +199,31 @@ Created on Wed Jan 25 22:37:05 2023
 #     longest = current
 
 # print(longest)
+
+# iteration = 0
+# count = 0
+# while iteration < 5:
+#     # the variable 'letter' in the loop stands for every 
+#     # character, including spaces and commas!
+#     for letter in "hello, world": 
+#         count += 1
+#     print("Iteration " + str(iteration) + "; count is: " + str(count))
+#     iteration += 1 
+
+# iteration = 0
+# while iteration < 5:
+#     count = 0
+#     for letter in "hello, world":
+#         count += 1
+#     print("Iteration " + str(iteration) + "; count is: " + str(count))
+#     iteration += 1 
+
+# iteration = 0
+# while iteration < 5:
+#     count = 0
+#     for letter in "hello, world":
+#         count += 1
+#         if iteration % 2 == 0:
+#             break
+#     print("Iteration " + str(iteration) + "; count is: " + str(count))
+#     iteration += 1 
