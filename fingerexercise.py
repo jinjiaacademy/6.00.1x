893a13d23b72ee5fca8fec6c17c61614c2e6f0c9
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-02-15 21:31:12 -0500

    Update fingerexercise.py

--- fingerexercise.py
+++ fingerexercise.py
@@ -6,3 +6,8 @@ Created on Sat Feb 11 13:22:33 2023
 @author: jinjialiu
 """
 
+"""
+Write a program that examines three variables - x, y, and z - and 
+prints the largest odd number among them. If none of them are
+odd, it should print the smallest value of the three.
+"""
