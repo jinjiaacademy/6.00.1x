570090c0b5d5206139741003d2576c6e9cb0c71f
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-03-03 13:40:02 -0500

    Create textbookcode.py

---
+++ textbookcode.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  3 12:26:30 2023
+
+@author: jinjialiu
+"""
+
+"""
+Figure 2-7 Squaring an integer, the hard way
+"""
+# x = 3
+# ans = 0
+# num_iterations = 0
+# while (num_iterations < x):
+#     ans = ans + x
+#     num_iterations = num_iterations + 1
+#     print(f'x={x}, ans={ans}, num_iterations={num_iterations}')
+# print(f'{x} * {x} = {ans}')
+
+# x = 3
+# ans = 0
+# for num_iterations in range(abs(x)):
+#     ans = ans + abs(x)
+# print(f'{x} * {x} = {ans}')
+
+# Find a positive integer that is divisible by both 11 and 12
+# x = 1
+# while True:
+#     print(x)
+#     if x % 11 == 0 and x % 12 == 0:
+#         break
+#     x += 1
+    
+# print(x, 'is divisible by 11 and 12.')
+
