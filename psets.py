50a3c206ab830413ae8ae04e01e640ee786d36cc
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-03-04 12:06:22 -0500

    Create psets.py

---
+++ psets.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  4 12:02:54 2023
+
+@author: jinjialiu
+"""
+
+""" PSET1 """
+
+"""
+Problem 1
+Assume s is a string of lower case characters.
+Write a program that counts up the number of vowels contained
+in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
+For example, if s = 'azcbobobegghakl', your program should print:
+
+Number of vowels: 5
+"""
+
+
+"""
+Problem 2
+Assume s is a string of lower case characters.
+Write a program that prints the number of times the string 'bob' 
+occurs in s. For example, if s = 'azcbobobegghakl', then your 
+program should print
+
+Number of times bob occurs is: 2
+"""
+
+
+"""
+Problem 3
+Assume s is a string of lower case characters.
+Write a program that prints the longest substring of s in which 
+the letters occur in alphabetical order. For example, 
+if s = 'azcbobobegghakl', then your program should print
+
+Longest substring in alphabetical order is: beggh
+
+In the case of ties, print the first substring. For example, 
+if s = 'abcbcd', then your program should print
+
+Longest substring in alphabetical order is: abc
+"""
