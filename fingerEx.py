9b6ec24b0bcc05193bec6d1006165be79e86a804
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-03-04 22:10:36 -0500

    Update fingerEx.py

--- fingerEx.py
+++ fingerEx.py
@@ -107,3 +107,91 @@ over the odd integers between 3 and 999.
         
 # print(total)
 
+"""
+Change the code in Figure 3-2 so that it returns the largest rather
+than the smallest divisor. Hint: if y*z = x and y is the smallest
+divisor of x, z is the largest divisor of x.
+"""
+
+# x = int(input('Enter an integer: '))
+# smallest_divisor = None
+
+# for guess in range(2, x):
+#     if x % guess == 0:
+#         smallest_divisor = guess
+#         break
+
+# if smallest_divisor != None:
+#     print('The largest divisor is', x/smallest_divisor)
+# else:
+#     print(x, 'is a prime number')
+    
+"""
+Write a program that asks the user to enter an integer and prints
+two integers: root and pwr, such that 1 < pwr < 6 and root**pwr
+is equal to the integer entered by the user. If no such pair of 
+integers exists, it should print a message to that effect.
+"""
+# x = int(input('Enter an integer: '))
+
+# for pwr in range(2, 6):
+#     for root in range(1, x):
+#         if root**pwr == x:
+#             print(f'{root} ** {pwr} = {x}')
+#             break
+# else:
+#     print('No such pair of integers exists.')
+
+"""
+Write a program that prints the sum of the prime numbers greater
+than 2 and less than 1000. Hint: you probably want to have a loop
+that is a primality test nested inside a loop that iterates over
+the odd integers between 3 and 999.
+"""
+# total = 0
+
+# for num in range(3, 1000, 2):
+#     is_prime = True
+#     for div in range(2, num):
+#         if num % div == 0:
+#             is_prime = False
+#             break
+#     if is_prime == True:
+#         total += num
+        
+# print(total)
+
+"""
+What would have to be changed to make the code in Figure 3-5 work
+for finding an approximation to the cube root of both negative and 
+positive numbers? 
+Hint: think about changing low to ensure that the answer lies within
+the region being searched.
+"""
+x = float(input("Enter a number: "))
+epsilon = 0.01
+low = min(-1.0, x)
+high = max(1.0, x)
+guess = (low + high) / 2.0
+num_guesses = 0
+
+while abs(guess ** 3 - x) >= epsilon:
+    if guess ** 3 < x:
+        low = guess
+    else:
+        high = guess
+    guess = (low + high) / 2.0
+    num_guesses += 1
+
+print("Number of guesses:", num_guesses)
+print("Approximation to the cube root of", x, "is:", guess)
+
+
+"""
+The Empire State Building is 102 stories high. A man want to know
+the highest floor from which he could drop an egg without the egg
+breaking. He proposed to drop an egg from the top floor. If it broke,
+he would go down a floor, and try it again. He would do this until
+the egg did not break. At worst, this method requires 102 eggs. 
+Implement a method that at worst uses seven eggs.
+"""
