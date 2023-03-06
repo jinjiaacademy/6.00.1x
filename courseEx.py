ef0d1d08c2f975bf7b930695196e8d81b2cf9feb
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-03-06 11:52:39 -0500

    Create courseEx.py

---
+++ courseEx.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  6 11:23:15 2023
+
+@author: jinjialiu
+"""
+
+# iteration = 0
+# count = 0
+# while iteration < 5:
+#     for letter in 'hello, world':
+#         count += 1
+#     print('Iteration ' + str(iteration) + '; count is: ', str(count))
+#     iteration += 1
+
+# iteration = 0
+# while iteration < 5:
+#     count = 0
+#     for letter in 'hello, world':
+#         count += 1
+#     print('Iteration ' + str(iteration) + '; count is: ', str(count))
+#     iteration += 1
+
+# iteration = 0
+# while iteration < 5:
+#     count = 0
+#     for letter in 'hello, world':
+#         count += 1
+#         if iteration % 2 == 0:
+#             break
+#     print('Iteration ' + str(iteration) + ': count is: ' + str(count))
+#     iteration += 1
+
+# x = 25
+# epsilon = 0.01
+# step = 0.1
+# guess = 0.0
+
+# while guess <= x:
+#     if abs(guess**2 - x) < epsilon:
+#         break
+#     else:
+#         guess += step
+
+# if abs(guess**2 - x) >= epsilon:
+#     print('failed')
+# else:
+#     print('succeeded: ' + str(guess))
+
+# x = 25
+# epsilon = 0.01
+# step = 0.1
+# guess = 0.0
+
+# while guess <= x:
+#     if abs(guess**2 - x) >= epsilon:
+#         guess += step
+
+# if abs(guess**2 - x) >= epsilon:
+#     print('failed')
+# else:
+#     print('succeeded: ' + str(guess))
+
+# x = 23
+# epsilon = 0.01
+# step = 0.1
+# guess = 0.0
+
+# while abs(guess**2 - x) >= epsilon:
+#     if guess <= x:
+#         guess += step
+#         print(guess)
+#     else:
+#         break
+
+# if abs(guess**2 - x) >= epsilon:
+#     print('failed')
+# else:
+#     print('succeeded: ' + str(guess))
